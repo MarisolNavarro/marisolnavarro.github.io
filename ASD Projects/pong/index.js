f7d1334af91728476d941d4d196b1571a7dd92b1
MarisolNavarro <navhedmar@gmail.com>
2020-11-27 17:34:17 +0000

    save message

--- ASD Projects/pong/index.js
+++ ASD Projects/pong/index.js
@@ -11,11 +11,17 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
   
   // Game Item Objects
-
+  var positionX = 0;
+   var speedX = 0;
+   var positionY = 0; 
+   var speedY = 0; 
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second) 
+  $(document).on('keydown', handleKeyDown);    
+
+  // change 'eventType' to the type of event you want to handle
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -33,15 +39,30 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+      
+        if (event.which === KEY.UP) {
+            speedY = -5;
+            console.log("up pressed");
+        }
+        else if (event.which === KEY.DOWN) {
+            speedY = 5;
+            console.log("down pressed");
 
-  }
+        }
+        
+
+    }   
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+    function repositionGameItem() {
+      positionX += speedX
+      positionY += speedY
+  }
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -49,5 +70,41 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+  function doCollide(ball, leftPaddle) {
+    // ball
+    ball.leftX = ball.x;
+    ball.topY = ball.y;
+  ball.rightX = ball.x + $(ball.id).width();
+	ball.bottomY = ball.y + $(ball.id).height();
+    
+    // board
+    leftPaddle.leftX = square2.x;
+    leftPaddle.topY = square2.y;
+    leftPaddle.rightX = square2.x + $(leftPaddle.id).width();
+	leftPaddle.bottomY = square2.y + $(leftPaddle.id).height();
+
+
+	if ((ball.rightX > leftPaddle.leftX) && 
+		(ball.leftX < leftPaddle.rightX) &&
+	    (ball.bottomY > leftPaddle.topY) &&
+	    (ball.topY < leftPaddle.bottomY)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+    function factoryFunction(id) {
+        var ball = {};
+        ball.id = id
+        ball.width = $(id).width();;
+        ball.height = $(id).height();
+        ball.x = Number($(id).css('left').replace(/[^-\d\.]/g, ''));
+        ball.y = Number($(id).css('top').replace(/[^-\d\.]/g, ''));
+        ball.speedX = 0
+        ball.speedY = 0
+        return ball;
+    }
   
 }
